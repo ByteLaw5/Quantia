5663c4897ab8fc12c0057a3dc4c3b1213a019565
ByteLaw5 <aradgal78@gmail.com>
2020-10-25 21:54:25 +0200

    Setup

---
+++ src/main/java/com/randomteam/Quantia.java
@@ -0,0 +1,12 @@
+package com.randomteam;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Quantia.MOD_ID)
+public class Quantia {
+    public static final String MOD_ID = "quantia";
+
+    public Quantia() {
+
+    }
+}
